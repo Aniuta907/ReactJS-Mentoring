fa6cd24fd28a3287750644b3501d9e38900c5af9
Anna Kuznetsova1 <anna_kuznetsova1@epam.com>
2021-03-10 11:33:28 +0300

    fix DeleteMovieModalContent.tsx

--- src/components/DeleteMovieModalContent/DeleteMovieModalContent.tsx
+++ src/components/DeleteMovieModalContent/DeleteMovieModalContent.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 import "./DeleteMovieModalContent.scss";
 
+const deleteMovieText = "Are you sure you want to delete this movie?";
 export class DeleteMovieModalContent extends React.Component {
-  deleteMovieText = "Are you sure you want to delete this movie?";
-
   render(): React.ReactNode {
-    return <p className="delete-movie-text">{this.deleteMovieText}</p>;
+    return <p className="delete-movie-text">{deleteMovieText}</p>;
   }
 }
