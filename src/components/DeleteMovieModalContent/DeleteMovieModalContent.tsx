0b2bc19b14aaf0051579c2c0a382a46dd23fb2c2
Anna Kuznetsova1 <anna_kuznetsova1@epam.com>
2021-03-10 23:14:54 +0300

    turn variable deleteMovieText into text

--- src/components/DeleteMovieModalContent/DeleteMovieModalContent.tsx
+++ src/components/DeleteMovieModalContent/DeleteMovieModalContent.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 import "./DeleteMovieModalContent.scss";
-
-const deleteMovieText = "Are you sure you want to delete this movie?";
 export class DeleteMovieModalContent extends React.Component {
   render(): React.ReactNode {
-    return <p className="delete-movie-text">{deleteMovieText}</p>;
+    return <p className="delete-movie-text">Are you sure you want to delete this movie?</p>;
   }
 }
